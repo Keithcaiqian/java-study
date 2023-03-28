add43a477ebc09d61cc22fe1cb461d0a49debfec
Keithcaiqian <653082189@qq.com>
2023-03-28 22:39:40 +0800

    项目练习

---
+++ puzzlegame/src/App.java
@@ -0,0 +1,11 @@
+import com.ljk.ui.GameJFrame;
+import com.ljk.ui.LoginJFrame;
+import com.ljk.ui.RegisterJFrame;
+
+public class App {
+    public static void main(String[] args) {
+        new LoginJFrame();
+        new GameJFrame();
+        new RegisterJFrame();
+    }
+}
---
+++ puzzlegame/src/com/ljk/ui/LoginJFrame.java
@@ -0,0 +1,10 @@
+package com.ljk.ui;
+
+import javax.swing.*;
+
+public class LoginJFrame extends JFrame {
+    public LoginJFrame(){
+        this.setSize(488, 433);
+        this.setVisible(true);
+    }
+}
