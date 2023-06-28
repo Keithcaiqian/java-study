03ab78462cabf3eb0245b68e213d7705d84ca686
lijiankun <653082189@qq.com>
2023-06-28 22:45:43 +0800

    feat:IOC&DI

---
+++ emp/src/main/java/com/example/controller/EmpController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.pojo.Emp;
+import com.example.pojo.Result;
+import com.example.service.EmpService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class EmpController {
+    @Resource
+    private EmpService empService ;
+
+    @RequestMapping("/listEmp")
+    public Result list(){
+        //1. 调用service, 获取数据
+        List<Emp> empList = empService.listEmp();
+
+        //3. 响应数据
+        return Result.success(empList);
+    }
+
+}
