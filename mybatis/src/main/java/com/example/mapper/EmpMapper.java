7606582a5ab79eee96753e5ff69b727ce423b5e8
lijiankun <653082189@qq.com>
2023-07-11 22:33:56 +0800

    mybatis完结

--- mybatis/src/main/java/com/example/mapper/EmpMapper.java
+++ mybatis/src/main/java/com/example/mapper/EmpMapper.java
@@ -3,11 +3,17 @@ package com.example.mapper;
 import com.example.pojo.Emp;
 import org.apache.ibatis.annotations.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface EmpMapper {
 
     @Delete("delete from emp where id = #{id}")
     public void delete(Integer id);
+
+    public void deleteByIds(List<Integer> ids);
 
     @Options(keyProperty = "id", useGeneratedKeys = true)
     @Insert("insert into emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time)" +
@@ -24,4 +30,29 @@ public interface EmpMapper {
             "    update_time = #{updateTime}" +
             "where id = #{id};")
     public void update(Emp emp);
+
+    public void update2(Emp emp);
+
+//    方案三： 开启mybatis的驼峰命名自动映射开关
+    @Select("select * from emp where id = #{id}")
+    public Emp getById(Integer id);
+
+    //方案一：    给字段起别名，让别名与实体类属性一致
+//    @Select("select id, username, password, name, gender, image, job, entrydate, " +
+//            "dept_id deptId, create_time createTime, update_time updateTime from emp where id = #{id}")
+//    public Emp getById(Integer id);
+
+//    方案二：通过@Results, @Result注解手动映射封装
+//    @Results({
+//            @Result(column = "dept_id", property = "deptId"),
+//            @Result(column = "create_time", property = "createTime"),
+//            @Result(column = "update_time", property = "updateTime"),
+//    })
+//    @Select("select * from emp where id = #{id}")
+//    public Emp getById(Integer id);
+
+//    @Select("select * from emp where name like concat('%', #{name}, '%') " +
+//            "and gender = #{gender} and entrydate between #{start} and #{end}")
+    public List<Emp> getList(String name, Short gender, LocalDate start, LocalDate end);
+
 }
--- mybatis/src/test/java/com/example/MybatisApplicationTests.java
+++ mybatis/src/test/java/com/example/MybatisApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class MybatisApplicationTests {
@@ -19,6 +21,12 @@ class MybatisApplicationTests {
     @Test
     public void testDelete(){
         empMapper.delete(17);
+    }
+
+    @Test
+    public void testDeleteByIds(){
+        List<Integer> list = Arrays.asList(18, 20, 21);
+        empMapper.deleteByIds(list);
     }
 
 
@@ -41,17 +49,30 @@ class MybatisApplicationTests {
     @Test
     public void testUpdate(){
         Emp emp = new Emp();
-        emp.setId(21);
-        emp.setUsername("Tom3");
-        emp.setName("汤姆3");
-        emp.setGender((short) 1);
-        emp.setImage("1.jpg");
-        emp.setJob((short) 1);
-        emp.setEntrydate(LocalDate.of(2000, 1, 1));
-        emp.setDeptId(1);
+        emp.setId(18);
+        emp.setUsername("Tom111");
+        emp.setName("汤姆111");
+//        emp.setGender((short) 1);
+//        emp.setImage("1.jpg");
+//        emp.setJob((short) 1);
+//        emp.setEntrydate(LocalDate.of(2000, 1, 1));
+//        emp.setDeptId(1);
         emp.setUpdateTime(LocalDateTime.now());
-        empMapper.update(emp);
+//        empMapper.update(emp);
+        empMapper.update2(emp);
     }
 
+    @Test
+    public void testSelect(){
+        Emp emp = empMapper.getById(6);
+        System.out.println(emp);
+    }
+
+    @Test
+    public  void testList(){
+//        List<Emp> empList = empMapper.getList("张", (short) 1, null,  null);
+        List<Emp> empList = empMapper.getList(null, null, null,  null);
+        System.out.println(empList);
+    }
 
 }
